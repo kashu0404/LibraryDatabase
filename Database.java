845484053247336a53e7000377fa859c34d90ed2
Kashmala Fareed <109161404+kashu0404@users.noreply.github.com>
2023-12-15 18:42:44 -0500

    Update Database.java

--- Database.java
+++ Database.java
@@ -68,5 +68,72 @@ public class Database {
         }   
     }
 
+    public void userMenu() {
+        int choice;
+
+        do {
+            System.out.println("1. Display Database");
+            System.out.println("2. Add Book");
+            System.out.println("3. Delete Book");
+            System.out.println("4. Search Book Using Author, Title or ISBN");
+            System.out.println("5. Update a Book");
+            System.out.println("0. Exit");
+
+            System.out.println("Enter your choice");
+            choice = scan.nextInt();
+
+            switch(choice) {
+                case 1:
+                    displayDatabase();
+                    break;
+                case 2:
+                    System.out.println("Enter Author Name: ");
+                    String author = scan.nextLine();
+                    System.out.println("Enter Title: ");
+                    String title = scan.nextLine();
+                    System.out.println("Enter ISBN: ");
+                    String is = scan.nextLine();
+                    System.out.println("Enter Publication Date: ");
+                    int date = scan.nextInt();
+
+                    addBook(new Books(title, author, date, is));
+
+                    break;
+                case 3:
+                    scan.nextLine();
+                    System.out.println("Enter Title to delete book: ");
+                    String str = scan.nextLine();
+                    deleteBook(new Books(str, "", 0, ""));
+                    break;
+                case 4 :
+                    System.out.println("Enter Author, Title or ISBN");
+                    String searchInput = scan.nextLine();
+                    searchBook(searchInput);
+                    break;
+                case 5:
+                    System.out.println("Enter the Updated Book (Title,Author,Date and ISBN):");
+                    System.out.println("Enter Author Name: ");
+                    String a = scan.nextLine();
+                    System.out.println("Enter the exact Title: ");
+                    String t = scan.nextLine();
+                    System.out.println("Enter exact ISBN: ");
+                    String i = scan.nextLine();
+                    System.out.println("Enter exact Publication Date: ");
+                    int d = scan.nextInt();
+
+                    updateDatabase(new Books(t, a, d, i));
+                    break;
+
+            }
+        } while (choice!=0);
+        
+    }
+
+    public static void main(String[] args) {
+        Database bookDatabase = new Database();
+        bookDatabase.userMenu();
+    }
+
+
 
 }
